070cbbe22ef2ce8e3d6a25df1bd4e8f441b95cb7
Asaki <37176460+yudingsxt@users.noreply.github.com>
2019-06-24 22:14:05 +0800

    Update weather.js

--- weather.js
+++ weather.js
@@ -8,13 +8,11 @@ $httpClient.get(weaapi, function(error, response, data){
     else {
         var obj = JSON.parse(data);
         console.log(obj);
-        var win_speed = obj.win_speed;
         var tem = obj.tem;
         var wea = obj.wea;
-        var city = obj.city;
         var time = "更新于："+obj.update_time;
-        let wmation = [tem,wea,time,city,win_speed];
-        $notification.post(wmation[2],wmation[3], wmation[1], wmation[0],wmation[4]);
+        let wmation = [tem,wea,time];
+        $notification.post(wmation[1], wmation[0], wmation[2]);
         $done();
     }
 }
