a25cff32346211477223e7fe3ad682311c49997f
Asaki <37176460+yudingsxt@users.noreply.github.com>
2019-06-26 13:50:47 +0800

    Update heweather.js

--- heweather.js
+++ heweather.js
@@ -22,7 +22,7 @@ $httpClient.get(hfapi, function(error, response, data){
     } else {
         var mm = getwmatioin(data);
         var title = "所在城市："+ mm[0];
-        var subtitle = "天气状况："+mm[1]+"  "+mm[3]+"  "+mm[5];
+        var subtitle = "天气状况："+mm[1]+"  "+mm[3]+"  "+mm[5]+"Km/s";
         var mation = "体感温度："+mm[2]+"℃"+"  "+"空气湿度："+mm[4]+"%"+"  "+ "\n降水量："+mm[6];
         $notification.post(title, subtitle, mation);
         $done();
