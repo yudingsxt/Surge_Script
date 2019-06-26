49c906e3d088837bd59a7d97d0d79987a89ba465
Asaki <37176460+yudingsxt@users.noreply.github.com>
2019-06-26 13:41:31 +0800

    Update heweather.js

--- heweather.js
+++ heweather.js
@@ -3,16 +3,14 @@ function getwmatioin(data){
     var obj = JSON.parse(data);
     console.log(obj);
     let city = obj.HeWeather6[0].basic["location"];
-    let updatetime = obj.HeWeather6[0].update["loc"];
     let wea = obj.HeWeather6[0].now["cond_txt"];
     let temp = obj.HeWeather6[0].now["fl"];
     let wind = obj.HeWeather6[0].now["wind_dir"];
     let wind_spd = obj.HeWeather6[0].now["wind_spd"];
     let pcpn = pbj.HEweather6[0].now["pcpn"];
     let hum = obj.HeWeather6[0].now["hum"];
-    let vis = obj.HeWeather6[0].now["vis"];
-    let mm = [city, wea, temp, wind, hum, vis, updatetime,wind_spd,pcpn];
-    return mm;
+    let mm = [city, wea, temp, wind, hum, wind_spd, pcpn];
+    return mm
 
 
 }
@@ -24,8 +22,8 @@ $httpClient.get(hfapi, function(error, response, data){
     } else {
         var mm = getwmatioin(data);
         var title = "所在城市："+ mm[0];
-        var subtitle = "天气状况："+mm[1]+"  "+mm[3]+"  "+mm[7];
-        var mation = "体感温度："+mm[2]+"℃"+"  "+"空气湿度："+mm[4]+"%"+"  "+"能见度："+mm[5]+"㎞"+ "\n降水量："+mm[6];
+        var subtitle = "天气状况："+mm[1]+"  "+mm[3]+"  "+mm[5];
+        var mation = "体感温度："+mm[2]+"℃"+"  "+"空气湿度："+mm[4]+"%"+"  "+ "\n降水量："+mm[6];
         $notification.post(title, subtitle, mation);
         $done();
     }
