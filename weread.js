85173c24c638886591d5d7a3f97d4a2763b2e0cf
Asaki <37176460+yudingsxt@users.noreply.github.com>
2019-07-01 20:05:38 +0800

    Update weread.js

--- weread.js
+++ weread.js
@@ -3,11 +3,8 @@ var url = $request.url;
 const path = "/pay/memberCardSummary";
 if (url.indexOf(path) != -1){
   let obj = JSON.parse(body);
-  obj.expiredTime= 1747624048;
-  obj.remainTime= 189302400;
-  obj.expired= 0;
   obj.hintsForRecharge.predictedChapterPrice=0;
-  obj.isAutoRenewable=1;
+  obj.expired=0;
   obj.permanent=1;
   body = JSON.stringify(obj);
 }
