472a0a03912cf0d037fb09e6f486ff5a48fb5de2
Asaki <37176460+yudingsxt@users.noreply.github.com>
2022-05-26 21:01:52 +0800

    Create ip.js

---
+++ ip.js
@@ -0,0 +1,27 @@
+let url = "https://ip.sb/api/"
+
+$httpClient.get(url, function(error, response, data){
+    let jsonData = JSON.parse(data)
+    let ip = jsonData.ip
+    let country = jsonData.country
+    let emoji = getFlagEmoji(jsonData.country_code)
+    let city = jsonData.city
+    let isp = jsonData.isp
+    
+  body = {
+    title: "网络信息",
+    content: `IP信息: ${ip}\n运营商: ${isp}\nIP位置: ${emoji}${country} - ${city}`,
+    icon: "link.icloud",
+    'icon-color': "#5AC8FA"
+  }
+  $done(body);
+});
+
+
+function getFlagEmoji(countryCode) {
+    const codePoints = countryCode
+      .toUpperCase()
+      .split('')
+      .map(char =>  127397 + char.charCodeAt());
+    return String.fromCodePoint(...codePoints);
+}
